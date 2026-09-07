e52238ee30e988e26430f198d895021647868cfb
JK-Tran <90900835+JK-Tran@users.noreply.github.com>
2026-09-07 13:24:42 +0700

    Fix NestFactory import path

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { NestFactory } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
